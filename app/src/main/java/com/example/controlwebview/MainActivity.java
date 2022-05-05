55e73b9ec01717f9576abd846bfc9a7d663f5a65
blackm4ss27 <84756153+blackm4ss27@users.noreply.github.com>
2022-05-05 13:47:02 -0500

    v1 de app de navegacion

---
+++ app/src/main/java/com/example/controlwebview/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.controlwebview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //enlazar la parte logica y diseño
+        et1 = (EditText)findViewById(R.id.txtWeb);
+    }
+
+    public void Navegar(View view) {
+        //se establecen lso cambios de pantallas
+        Intent i = new Intent(this,Web.class);
+        //enviar parametrosd intent, metodo put extra
+        i.putExtra("SitioWeb", et1.getText().toString());
+        startActivity(i);
+    }
+}
